267e451d5a47f07b5d101a7ff1b8edb207745c21
jhonierp <jhonierpasos9@gmail.com>
2023-11-22 22:15:18 -0500

    conexion de api en tabla productos

--- src/layouts/Tabla Inventario/index.js
+++ src/layouts/Tabla Inventario/index.js
@@ -1,21 +1,41 @@
+import React, { useEffect, useState } from "react";
 import Card from "@mui/material/Card";
 import SoftBox from "components/SoftBox";
 import SoftTypography from "components/SoftTypography";
-
-
 import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
 import DashboardNavbar from "examples/Navbars/DashboardNavbar";
 import Footer from "examples/Footer";
 import Table from "examples/Tables/Table";
 
-
-import authorsTableData from "layouts/tables/data/authorsTableData";
-import projectsTableData from "layouts/tables/data/projectsTableData";
-import data_productos from "./data_productos/data_productos";
+const data_productos = {
+  columns: [
+    { name: "id", align: "left" },
+    { name: "nombre", align: "left" },
+    { name: "descripcion", align: "left" },
+    { name: "categoria", align: "center" },
+    { name: "precio", align: "center" },
+    { name: "estado", align: "center" },
+    { name: "fecha_vencimiento", align: "center" },
+  ],
+};
 
 function Tabla_Inventario() {
-  const { columns, rows } = data_productos;
-  
+  const { columns } = data_productos;
+  const [productos, setProductos] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("https://diplomadobd-06369030a7e4.herokuapp.com/productos/");
+        const data = await response.json();
+        setProductos(data); // Actualiza el estado con los datos de la API
+      } catch (error) {
+        console.error("Error al obtener datos de la API", error);
+      }
+    };
+
+    fetchData(); // Llama a la función para obtener datos cuando el componente se monta
+  }, []); // El segundo parámetro vacío asegura que useEffect se ejecute solo una vez al montar el componente
 
   return (
     <DashboardLayout>
@@ -36,13 +56,12 @@ function Tabla_Inventario() {
                 },
               }}
             >
-              <Table columns={columns} rows={rows} />
+              <Table columns={columns} rows={productos} />
             </SoftBox>
           </Card>
         </SoftBox>
-        
       </SoftBox>
-     
+      <Footer />
     </DashboardLayout>
   );
 }
